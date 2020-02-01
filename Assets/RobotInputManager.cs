df7e91de5c80f5edc6d62547beb37d791282aa25
DrDust0 <salvatoresolimeno70@gmail.com>
2020-02-01 03:20:54 +0100

    Started the robot parts stuff

---
+++ Assets/RobotInputManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotInputManager : MonoBehaviour
+{ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckInputs(List<RobotPart> parts)
+    {
+        foreach(var part in parts)
+        {
+
+            if(part.isAttached)
+            {
+                if(part.attachedInput.inputType == InputType.axis)
+                {
+                    var x = Input.GetAxis("Horizontal");
+                    var z = Input.GetAxis("Vertical");
+                    if((x != 0 || z != 0))
+                    {
+                        part.AttachedAction(x, z);
+                    }
+                }
+                else
+                {
+                    if(Input.GetKey(part.attachedInput.key))
+                    {
+                        part.AttachedAction(1, 0);
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
