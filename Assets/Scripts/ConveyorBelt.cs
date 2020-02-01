ec46ec615a5bdfae54f5b259b846a0edbd2cce94
Martino Fontana <tinozzo123@gmail.com>
2020-02-01 23:10:21 +0100

    ConveyorBelt overhaul

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -8,11 +8,22 @@ public class ConveyorBelt : Activatable
 
     public void invert() {
         speed *= -1;
+        adaptAnimation();
+    }
+
+    void adaptAnimation() {
+        var renderer = GetComponentsInChildren<Renderer>()[0];
+        const string propertyName = "Vector1_ECD6E8FA";
+        renderer.material.SetFloat(propertyName, speed.x / 20);
     }
 
     public void OnCollisionStay(Collision other) {
         if (active) {
             other.gameObject.GetComponent<Rigidbody>().AddForce(speed);
         }
+    }
+
+    void Start() {
+        adaptAnimation();
     }
 }
