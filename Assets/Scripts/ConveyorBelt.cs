93e247afd8dd99653baf00cb424ef2805b1422c4
Martino Fontana <tinozzo123@gmail.com>
2020-02-01 11:50:06 +0100

    ConveyrBelt removed time

--- Assets/Scripts/ConveyorBelt.cs
+++ Assets/Scripts/ConveyorBelt.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class ConveyorBelt : MonoBehaviour
 {
     public Vector3 speed;
-    public float timeToInvert;
 
     public void invert() {
         speed *= -1;
     }
 
     public void OnCollisionStay(Collision other) {
+
         other.gameObject.GetComponent<Rigidbody>().AddForce(speed);
     }
 
