aff8d590d12fa8bb94b318a6535864b855abe9e2
Martino Fontana <tinozzo123@gmail.com>
2020-02-01 11:00:34 +0100

    Basic conveyor belt script

---
+++ Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public Vector3 speed;
+
+    public void OnCollisionStay(Collision other) {
+        var otherRb = other.gameObject.GetComponent<Rigidbody>();
+        otherRb.velocity = speed;
+    }
+
+    public void OnCollisionExit(Collision other) {
+        var otherRb = other.gameObject.GetComponent<Rigidbody>();
+        otherRb.velocity = Vector3.zero;
+    }
+}
