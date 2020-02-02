815ad65176fc3b8cf7924d7b8f6b8be5beafce39
Martino Fontana <tinozzo123@gmail.com>
2020-02-02 12:22:23 +0100

    Door now disable some components instad of itself (solves bug that it didn't close)

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Door : Activatable
 {
+    private MeshRenderer mr;
+    private Collider c;
+    void Start() {
+        mr = GetComponent<MeshRenderer>();
+        c = GetComponent<Collider>();
+    }
+
     void Update()
     {
         if (active) {
-            gameObject.SetActive(false);
+            open();
         }
+        else {
+            close();
+        }
+    }
+
+    void open() {
+        mr.enabled = false;
+        c.enabled = false;
+    }
+
+    void close() {
+        mr.enabled = true;
+        c.enabled = true;
     }
 }
