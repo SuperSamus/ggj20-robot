85b472dbb96049131840be4ef91236074f8b4a46
Martino Fontana <tinozzo123@gmail.com>
2020-02-02 11:25:55 +0100

    Camera fade and ending script

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<FadeCamera>().end();
+    }
+}
